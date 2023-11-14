db81705f2318c8e8a56027a376ac93b9c156e921
Jhonny_Torres <128723713+Saber235@users.noreply.github.com>
2023-11-14 14:10:35 -0500

    Update Operaciones.py

--- ADSO 14/Operaciones.py
+++ ADSO 14/Operaciones.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 14 12:01:25 2023
+Programacion orientada a obgetos"___________"
 
-@author: FORMACION
+
 """
-
 class Persona:
     
     def imprimirNombre():
          print("Sapo")
          
-    def sumararNumero():
+    def sumarNumeros():
         print("Introduzca dos números para sumarlos")
         numero1 = input("Introduzca el primer número: ")
         numero2 = input("Introduzca el segundo número: ")
@@ -21,13 +20,10 @@ class Geometria:
     def areaTriangulo():    
        print("Introduzca la Base y la Altura del Triangulo")
        Base = input("Introduzca el numero de la base:")
-       Altura = imput("Introduzca el numero de la altura:")
-       opera = int(Base) * int(Altura)/2
-       print ("El area del triangulo es:",opera)
-       
-       
-       
-       
-Geometria. areaTriangulo()         
+       Altura = input("Introduzca el numero de la altura:")
+       opera = int(Base) * int(Altura) / 2
+       print("El area del triangulo es:",opera)
+
+Geometria.areaTriangulo()         
 Persona.imprimirNombre()     
-Persona.sumararNumero()
+Persona.sumarNumeros()
